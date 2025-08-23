cbe6d58175b0a979696394d4a96c6e09f93746a1
Devendra Singh <devthedeveloperguy@gmail.com>
2025-08-23 12:39:05 +0530

    init: godspeed, little repo

---
+++ extension.js
@@ -0,0 +1,9 @@
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+
+export default class PowerDialExtension extends Extension {
+    enable() {
+    }
+
+    disable() {
+    }
+}
